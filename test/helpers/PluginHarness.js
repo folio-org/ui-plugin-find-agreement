91c397940882d47d50b5fb46e6b5cd869f2f676b
Aditya Matukumalli <aditya.matukumalli@gmail.com>
2021-07-26 17:37:10 -0400

    Refactor Plugin harness to functional component

--- test/helpers/PluginHarness.js
+++ test/helpers/PluginHarness.js
@@ -4,45 +4,43 @@ import { Button } from '@folio/stripes/components';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 
-class PluginHarness extends React.Component {
-  static propTypes = {
-    onAgreementSelected: PropTypes.func,
-  }
+const PluginHarness = ({ onAgreementSelected = () => { }, ...rest }) => {
+  return (
+    <Pluggable
+      aria-haspopup="true"
+      dataKey="agreements"
+      id="clickable-find-agreement"
+      marginTop0
+      onAgreementSelected={onAgreementSelected}
+      renderTrigger={(props) => {
+        const buttonProps = {
+          'aria-haspopup': 'true',
+          'buttonStyle': 'primary',
+          'id': 'find-agreement-btn',
+          'name': 'dummy',
+          'onClick': props.onClick,
+          'marginBottom0': true
+        };
 
-  render() {
-    return (
-      <Pluggable
-        aria-haspopup="true"
-        dataKey="agreements"
-        id="clickable-find-agreement"
-        marginTop0
-        onAgreementSelected={this.props.onAgreementSelected}
-        renderTrigger={(props) => {
-          const buttonProps = {
-            'aria-haspopup': 'true',
-            'buttonStyle': 'primary',
-            'id': 'find-agreement-btn',
-            'name': 'dummy',
-            'onClick': props.onClick,
-            'marginBottom0': true
-          };
+        return (
+          <Button
+            data-test-plugin-agreement-button
+            {...buttonProps}
+          >
+            <FormattedMessage id="ui-plugin-find-agreement.selectAgreement" />
+          </Button>
+        );
+      }}
+      searchButtonStyle="link"
+      searchLabel="Look up agreements"
+      type="find-agreement"
+      {...rest}
+    />
+  );
+}
 
-          return (
-            <Button
-              data-test-plugin-agreement-button
-              {...buttonProps}
-            >
-              <FormattedMessage id="ui-plugin-find-agreement.selectAgreement" />
-            </Button>
-          );
-        }}
-        searchButtonStyle="link"
-        searchLabel="Look up agreements"
-        type="find-agreement"
-        {...this.props}
-      />
-    );
-  }
+PluginHarness.propTypes = {
+  onAgreementSelected: PropTypes.func
 }
 
 export default PluginHarness;
