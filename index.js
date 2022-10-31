91e86db656613f2dd0aabca730e7e5fcb54df65e
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-10-31 17:21:32 +0000

    ERM-1304, Add test coverage for AgreementSearch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import React from 'react';
-import AgreementSearch from './src/AgreementSearch';
+import AgreementSearch from './src/AgreementSearch/AgreementSearch';
 
 export default props => <AgreementSearch {...props} />;
