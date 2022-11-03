97e079b8274a9630de797406fb474238268ffd21
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-11-03 09:55:09 +0000

    Add index.js file for Filters component

--- src/View.js
+++ src/View.js
@@ -22,7 +22,7 @@ import {
   SearchAndSortSearchButton as FilterPaneToggle,
 } from '@folio/stripes/smart-components';
 
-import Filters from './Filters/Filters';
+import Filters from './Filters';
 import css from './View.css';
 
 const Agreements = ({
